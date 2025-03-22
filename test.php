1b4d4b8827d3908d43161dfe4aae821530453725
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-03-22 20:12:55 +0100

    Initial commit

--- test.php
+++ test.php
@@ -2,25 +2,12 @@
 require 'send.php';
 require 'inc/drc.php'; // Load DRC  
 
-// if (function_exists('mail')) {
-//     echo "PHP mail() function is enabled!";
-// } else {
-//     echo "PHP mail() function is disabled on this server.";
-// }
+
 
 $fname = 'Isaac';
 $to = "ioadejuwon@gmail.com"; // Replace with your email
 $subject = "Registration Successfull 🎉";
 $replyTo = BRAND_EMAIL;
-
-// if (mail($to, $subject, $message, $headers)) {
-//     echo "Mail sent successfully!";
-// } else {
-//     echo "Mail sending failed. Check server configuration.";
-//     error_log(print_r(error_get_last(), true)); // Log the error for debugging
-// }
-
-
 
 
 $response = [];
@@ -33,7 +20,10 @@ $emailSent = sendEmail(
     [
         'FIRST_NAME' => $fname,
         'YEAR' => FOOTERYEAR
-    ]
+    ],
+    $from,
+    $fromName,
+    $replyTo
 );
 
 if ($emailSent) {
